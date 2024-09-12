1fbe4a5d0ebc11a897e093991bd1788615d880e0
Laptov Sergey <laptov@1221systems.ru>
2024-09-12 22:09:05 +0500

    commit name

--- counter.js
+++ counter.js
@@ -2,10 +2,14 @@ export function setupCounter(element) {
   let counter = 0
 
   const setCounter = (count) => {
+
     counter = count
 
-    element.innerHTML = `count is ${counter}`
+
+
+    element.innerHTML = `counter is ${counter}`
   }
+
 
   element.addEventListener('click', () => setCounter(counter + 1))
 
